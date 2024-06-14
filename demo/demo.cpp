46de40a8a362127fb6261ca7340ffd0ad4b1dd71
Shuji Urano <urano_shuji@cyberagent.co.jp>
2024-06-14 22:16:33 +0900

    Add main function

---
+++ demo/demo.cpp
@@ -0,0 +1,17 @@
+﻿// demo.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <opencv2/opencv.hpp>
+#include "../include/YOLOX.h"
+
+int main(int argc, char* argv[])
+{
+    cv::Mat image;
+    cv::VideoCapture capture;
+
+    switch (argc)
+    {
+
+    }
+}
+
