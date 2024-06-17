bd141fa4da2ad6dca2d4f330efdf0c8e379eb8c2
Shuji Urano <urano_shuji@cyberagent.co.jp>
2024-06-17 14:00:26 +0900

    Add YOLOv9 folder

---
+++ YOLOv9/main.cpp
@@ -0,0 +1,16 @@
+// demo.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <opencv2/opencv.hpp>
+#include "YOLOv9.hpp"
+
+int main(int argc, char* argv[])
+{
+    cv::Mat image;
+    cv::VideoCapture capture;
+
+    switch (argc)
+    {
+
+    }
+}
