433fa393ab8afea68bef608401e055b7aff47ef6
Shuji Urano <urano_shuji@cyberagent.co.jp>
2024-09-06 22:38:32 +0900

    マージ

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -4,17 +4,80 @@
 #include <opencv2/opencv.hpp>
 #include "../include/YOLOX.h"
 
+#if __cplusplus >= 201703L
+#include <filesystem>
+namespace fs = std::filesystem;
+bool FileExists(const char* path) { return fs::exists(path); }
+#else
+#include <sys/stat.h>
+bool FileExists(const char *name)
+{
+  struct stat   buffer;
+  return (stat (name, &buffer) == 0);
+}
+#endif
+
+#define ENGINE_FILE "../models/yolox_s.engine"
+#define MODEL_FILE "../models/yolox_s.onnx"
+#define MODEL_WIDTH 640
+#define MODEL_HEIGHT 640
+
+//const char* labels[] = 
+//{
+//    #include "../include/coco_80_labels.h"
+//};
+
 int main(int argc, char* argv[])
 {
     cv::Mat image;
     cv::VideoCapture capture;
     YOLOX yolox;
 
+    YOLOX yolo;
+
     switch (argc)
     {
     case 3:
-        capture.open(argv[2]);
-        yolox.LoadEngine(argv[1], 640, 640);
+        if (!FileExists(ENGINE_FILE))
+        {
+            printf("Convert ONNX to Engine: %s\n", MODEL_FILE);
+            if (yolo.LoadModel(argv[1], MODEL_WIDTH, MODEL_HEIGHT))
+            {
+                yolo.SaveEngine(ENGINE_FILE);
+            }
+        }
+        else
+        { 
+            yolo.LoadEngine(ENGINE_FILE, MODEL_WIDTH, MODEL_HEIGHT);
+        }
+        break;
+    default:
+        puts("Usage: YOLOX <model> <input image>");
+        return 0;
+    }
+
+    if (capture.open(argv[2]))
+    {
+        for (bool loop = true; loop && capture.read(image); )
+        {
+            cv::imshow("Image", image);
+            yolo.Detect(image);
+            yolo.DrawObjects(image, labels);
+            cv::imshow("Image", image);
+            switch (cv::waitKey(1))
+            {
+            case 'q':
+                loop = false;
+                break;
+
+            case ' ':
+                break;
+            }
+        }
+    }
+    else
+    {
+        printf("Can't open %s\n", argv[2]);
     }
 }
 
