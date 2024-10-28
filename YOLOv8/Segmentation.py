4dc063506aebc8eb6c9a7cd65471907573f32862
Hiroshi Matsuoka <pipomaomy2005@gmail.com>
2024-10-28 21:52:29 +0900

    Add Arguments

--- YOLOv8/Segmentation.py
+++ YOLOv8/Segmentation.py
@@ -6,7 +6,7 @@ def Inference(config):
     file = os.path.join(config.path, config.filename)
     print(file)
     model = YOLO('yolov8m-seg.pt')
-    results = model(file)
+    results = model.predict(source=file, imgsz=config.size)
     for r in results:
         print(r.masks)
 
@@ -14,6 +14,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--filename', default='E_7BUVjUYAQ3UHx.jpg')
     parser.add_argument('-p', '--path', default='../images')
+    parser.add_argument('-s', '--size', default=480)
     Inference(parser.parse_args())
 
 
