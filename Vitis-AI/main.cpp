90855eac168953b3451d46db4e1facdc157c08b3
Shuji Urano <urano_shuji@cyberagent.co.jp>
2024-11-12 16:04:02 +0900

    推論テスト

--- Vitis-AI/main.cpp
+++ Vitis-AI/main.cpp
@@ -12,6 +12,20 @@ int main(int argc, char* argv[])
     vitis::YOLOv8 yolo;
     if (yolo.create(MODEL))
     {
+        cv::Mat image;
+        if (2 <= argc)
+        {
+            image = cv::imread(argv[1]);
+            vitis::YOLOv8Result results = yolo.run(image);
+            for (const auto& result : results.bboxes)
+            {
+                int label = result.label;
+                auto& box = result.box;
+                cv::rectangle(image, cv::Point(box[0], box[1]), cv::Point(box[2], box[3]), cv::Scalar());
+                cv::imshow("result", image);
+                cv::waitKey(0);
+            }
+        }
         puts(MODEL.c_str());
     }
 }
